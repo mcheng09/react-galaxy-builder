a34d15c3fdb4effa5f2151da004f33f618f480d1
Michael <mcheng.oc@gmail.com>
2019-12-28 22:44:10 -0800

    create(GalaxyBuilder): creates GalaxyBuilder container and adds to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-import Layout from './components/layout/layout'
+import Layout from './components/Layout/Layout';
+import GalaxyBuilder from './containers/GalaxyBuilder/GalaxyBuilder';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
         <Layout />
+        <GalaxyBuilder />
       </div>
     );
   }
---
+++ src/containers/GalaxyBuilder/GalaxyBuilder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GalaxyBuilder extends Component {
+  render () {
+    return (
+      <div>Galaxy Builder Component</div>
+    )
+  }
+}
+
+export default GalaxyBuilder;
