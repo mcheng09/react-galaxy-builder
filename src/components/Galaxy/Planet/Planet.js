0ef86a1091e9e4345a8fdd9ff3d78aea2db5b722
Michael <mcheng.oc@gmail.com>
2019-12-29 18:41:50 -0800

    updates(Planet): imports component style and adds switch case

--- src/components/Galaxy/Planet/Planet.js
+++ src/components/Galaxy/Planet/Planet.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import style from './Planet.module.css'
+
 const planet = (props) => {
-  return (
-    <div>Planet Component</div>
-  )
+  let planet = null;
+
+  switch(props.type) {
+    case ('mercury'):
+      planet = <div className={style.Mercury}></div>;
+      break;
+
+    case ('mars'):
+      planet = <div className={style.Mars}></div>;
+      break;
+
+    case ('jupiter'):
+      planet = <div className={style.Jupiter}></div>;
+      break;
+
+    case ('saturn'):
+      planet = <div className={style.Saturn}></div>;
+      break;
+
+    default:
+        planet = null;
+  }
+
+  return planet;
+
 }
 
 export default planet;
