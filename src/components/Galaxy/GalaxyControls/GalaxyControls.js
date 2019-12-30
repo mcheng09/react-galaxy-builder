2323e14f3d7949c2ccf2a308e07bfb0da10d860c
Michael <mcheng.oc@gmail.com>
2019-12-30 15:33:45 -0800

    feat(GalaxyControls): adds map function to render all planets

--- src/components/Galaxy/GalaxyControls/GalaxyControls.js
+++ src/components/Galaxy/GalaxyControls/GalaxyControls.js
@@ -3,10 +3,19 @@ import React from 'react';
 import GalaxyControl from './GalaxyControl/GalaxyControl'
 import style from './GalaxyControls.module.css';
 
+const controls = [
+  { label: 'Mercury', type:'mercury'},
+  { label: 'Mars', type:'mars'},
+  { label: 'Jupiter', type:'jupiter'},
+  { label: 'Saturn', type:'saturn'}
+]
+
 const GalaxyControls = (props) => {
   return (
     <div className={style.GalaxyControls}>
-      <GalaxyControl />
+      { controls.map((control, i) => {
+        return <GalaxyControl label={control.label} type={control.type} key={control.label + i}/>
+      })}
     </div>
   )
 }
