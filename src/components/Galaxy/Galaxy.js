d2b1a0c3957da25c45a20fd61f280ff11bcae438
Michael <mcheng.oc@gmail.com>
2019-12-29 19:42:36 -0800

    feat(Galaxy): flattens Planet array and adds condition to check if empty

--- src/components/Galaxy/Galaxy.js
+++ src/components/Galaxy/Galaxy.js
@@ -4,12 +4,19 @@ import Planet from './Planet/Planet'
 
 const galaxy = (props) => {
 
-  const transformedPlanets = Object.keys(props.planets)
+  let transformedPlanets = Object.keys(props.planets)
     .map(planet => {
       return [...Array(props.planets[planet])].map((_, i) => {
         return <Planet key={planet + i} type={planet} />
       });
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+    if (transformedPlanets.length < 1) {
+      transformedPlanets = <p>Start Building Your Galaxy!</p>
+    }
 
   return (
     <div className='Galaxy'>
