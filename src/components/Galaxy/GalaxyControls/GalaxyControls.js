0b08f0ab4c83287ba82832beaa707501bd99ea6b
Michael <mcheng.oc@gmail.com>
2019-12-30 14:56:01 -0800

    updates(GalaxyControls): adds simple css

--- src/components/Galaxy/GalaxyControls/GalaxyControls.js
+++ src/components/Galaxy/GalaxyControls/GalaxyControls.js
@@ -4,7 +4,13 @@ import style from './GalaxyControls.module.css';
 
 const GalaxyControls = (props) => {
   return (
-    <div className={style.GalaxyControls}></div>
+    <div className={style.GalaxyControls}>
+      <div className={style.Container}>
+        <div className={style.Label}>{props.label}</div>
+        <button>Remove</button>
+        <button>Add</button>
+      </div>
+    </div>
   )
 }
 
