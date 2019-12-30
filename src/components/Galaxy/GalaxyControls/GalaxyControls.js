7d573ed8ba23bf0a221adc1ed3230f0ddeeef766
Michael <mcheng.oc@gmail.com>
2019-12-30 15:10:16 -0800

    updates(GalaxyControls): removes markup and css transferred to GalaxyControl

--- src/components/Galaxy/GalaxyControls/GalaxyControls.js
+++ src/components/Galaxy/GalaxyControls/GalaxyControls.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
+import GalaxyControl from './GalaxyControl/GalaxyControl'
 import style from './GalaxyControls.module.css';
 
 const GalaxyControls = (props) => {
   return (
     <div className={style.GalaxyControls}>
-      <div className={style.Container}>
-        <div className={style.Label}>{props.label}</div>
-        <button>Remove</button>
-        <button>Add</button>
-      </div>
+      <GalaxyControl />
     </div>
   )
 }
