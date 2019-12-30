6871e9591f184128e8ab0b4bd442fc7a0ecba770
Michael <mcheng.oc@gmail.com>
2019-12-29 19:24:48 -0800

    feat(GalaxyBuilder): adds state for planets

--- src/containers/GalaxyBuilder/GalaxyBuilder.js
+++ src/containers/GalaxyBuilder/GalaxyBuilder.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react';
 import Galaxy from '../../components/Galaxy/Galaxy'
 
 class GalaxyBuilder extends Component {
+
+  state = {
+    planets: {
+      mercury: 1,
+      mars: 2,
+      jupiter: 1,
+      saturn: 1
+    }
+  }
+
   render () {
     return (
         <div>
           <h1>Galaxy Builder Component</h1>
-          <Galaxy />
+          <Galaxy planets={this.state.planets}/>
         </div>
     )
   }
